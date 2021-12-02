befea3451a2bac501482c1cc9262fbfda4c8bb67
Moises Rivas <moises.rivas@metrodev.com.ar>
2021-12-02 20:19:29 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+app.MapGet("/api/users", () => {
+
+    var user1 = new User("Enrique", "Diaz");
+    var user2 = new User("Lionel", "Messi");
+    
+    var users = new List<User>();
+
+    users.Add(user1);
+    users.Add(user2);
+
+    return users;
+
+});
+
+app.Run();
---
+++ User.cs
@@ -0,0 +1,22 @@
+public class User
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+
+
+    public User()
+    {
+
+    }
+
+    public User(string name, string surname)
+    {
+        Name = name;
+        Surname = surname;        
+    }
+
+    public string FullName()
+    {
+        return $"{Name} {Surname}";
+    }
+}
